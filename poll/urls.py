af4ef0621444527154ec7c3f44ebacfc95294df5
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-04-11 22:28:36 +0300

    update db and add some staff for rating

--- poll/urls.py
+++ poll/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home, assessment
 
 urlpatterns = [
     path('', home),
+    path('1/', assessment),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from .models import TeacherLectureAssessment
+from django.urls import reverse_lazy
+from .forms import TeacherLectureAssessmentForm
 def home(request):
     return render(request, 'home/index.html')
+
+def assessment(request):
+    return render(request, 'assessment/index.html', context={'form':TeacherLectureAssessmentForm})
