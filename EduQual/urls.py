22e72a0e5f8deb2ff4fd67ae5b4a476ff18858b0
deltaonlyone <deltaonlyone@gmail.com>
2023-04-12 01:04:16 +0300

    fix all issues connected with login

--- EduQual/urls.py
+++ EduQual/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('users.urls')),
     path('accounts/', include('users.urls')),
     path('', include('poll.urls')),
 ]
