6d11be2b3e5775dae5b75692a79d9e3cbf2aeb29
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-04-11 19:34:21 +0300

    url for home but without images

--- EduQual/urls.py
+++ EduQual/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('login/', include('users.urls')),
+    path('', include('poll.urls')),
 ]
